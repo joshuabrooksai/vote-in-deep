f1b0a13d75899a3cc6ce3ffbaf3c633e5a66958e
joshuabrooksai <WetzellLemarie6253@hotmail.com>
2025-10-18 22:34:19 +0800

    deploy setup

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedEncryptedVoting = await deploy("EncryptedVoting", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`EncryptedVoting contract: `, deployedEncryptedVoting.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_encrypted_voting"; // id required to prevent reexecution
+func.tags = ["EncryptedVoting"];
